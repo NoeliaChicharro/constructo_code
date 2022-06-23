713d923a6b8f7639079ea6a5f93f4df07e31b6ba
devops <devops@marisma.net>
2022-06-23 16:49:52 +0200

    create dialog to add new student
    add column in grid to delete user

--- frontend/src/main/java/ch/constructo/frontend/views/teachers/StudentListView.java
+++ frontend/src/main/java/ch/constructo/frontend/views/teachers/StudentListView.java
@@ -12,7 +12,12 @@ import ch.constructo.frontend.views.MainLayout;
 import ch.constructo.frontend.views.MainViewFrame;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.Div;
@@ -20,8 +25,13 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
@@ -31,6 +41,7 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -47,9 +58,24 @@ public class StudentListView extends MainViewFrame {
 
   //protected PatternForm form;
   private Grid<User> grid;
-  protected TextField searchField = new TextField();
-  protected Button addStudentButton = new Button();
+  private TextField searchField;
+  private Button addStudentButton;
+  private Dialog dialog;
   List<User> students;
+
+  // Form Dialog
+  FormLayout formLayout = new FormLayout();
+  TextField firstName = new TextField("Vorname");
+  TextField lastName = new TextField("Nachname");
+  TextField username = new TextField("Benutzername");
+  TextField email = new TextField("Email");
+  TextField password = new TextField("Passwort");
+  ComboBox<Role> role = new ComboBox<>("Rolle");
+
+  private Button save = new Button(VaadinIcon.SAFE.create());
+  private Button cancel = new Button("Abbrechen");
+
+  private Binder<User> binder = new BeanValidationBinder<>(User.class);
 
   @Override
   protected void onAttach(AttachEvent attachEvent) {
@@ -57,8 +83,6 @@ public class StudentListView extends MainViewFrame {
     initAppBar();
     executeFindAllUsers();
     setViewContent(createContent());
-    //refreshGrid();
-    //filter();
   }
 
   private void initAppBar(){
@@ -85,6 +109,98 @@ public class StudentListView extends MainViewFrame {
     return content;
   }
 
+  private Component addButtonDialog(){
+    addStudentButton = new Button(VaadinIcon.PLUS.create());
+    setupDialog();
+
+    addStudentButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    addStudentButton.addClickListener(e -> {
+      dialog.open();
+    });
+
+    return addStudentButton;
+  }
+
+  private Dialog setupDialog(){
+    dialog = new Dialog();
+    dialog.add(new Label("Schüler hinzufügen"));
+    dialog.add(createForm(), createButtonLayout());
+
+    binder.bindInstanceFields(this);
+    binder.bind(firstName, "firstName");
+    binder.bind(lastName, "lastName");
+    binder.bind(username, "username");
+    binder.bind(role, "role");
+    binder.bind(password, "password");
+
+    return dialog;
+  }
+
+  private FormLayout createForm(){
+    formLayout = new FormLayout();
+    String generatedUsername;
+    role.setItems(Role.values());
+
+    firstName.setRequired(true);
+    lastName.setRequired(true);
+    username.setEnabled(false);
+    password.setEnabled(false);
+
+    firstName.addValueChangeListener(e -> {
+      String input = firstName.getValue().trim();
+      String firstLetter = String.valueOf(input.charAt(0));
+      username.setValue(firstLetter.toLowerCase());
+    });
+
+    lastName.addValueChangeListener(e -> {
+      String input = lastName.getValue().trim();
+      String firstLetter = String.valueOf(input.charAt(0));
+      String secondLetter = String.valueOf(input.charAt(1));
+      username.setValue(username.getValue() + firstLetter.toLowerCase() + secondLetter.toLowerCase());
+    });
+
+    SecureRandom secureRandom = new SecureRandom();
+    password.setValue(secureRandom.toString());
+
+    formLayout.add(firstName, lastName, username, email, password);
+    return formLayout;
+  }
+
+  private HorizontalLayout createButtonLayout(){
+    addClassName("button-layout");
+    HorizontalLayout layout = new HorizontalLayout();
+    layout.getStyle().set("margin-bottom", "20px");
+    save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    cancel.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
+    save.addClickShortcut(Key.ENTER);
+    cancel.addClickShortcut(Key.ESCAPE);
+
+    save.addClickListener(e -> {
+      if (binder.isValid()){
+        User user = new User();
+        user.setFirstName(firstName.getValue());
+        user.setLastName(lastName.getValue());
+        user.setUsername(username.getValue());
+        user.setPassword(password.getValue());
+        user.setEmail(email.getValue());
+        user.setRole(Role.STUDENT);
+        try {
+          binder.writeBean(user);
+          userService.save(user);
+          students.add(user);
+        } catch (ValidationException validationException) {
+          validationException.printStackTrace();
+        }
+        Notification.show("Schüler gespeichert");
+        dialog.close();
+      }
+    });
+    cancel.addClickListener(e -> {dialog.close();});
+
+    layout.add(save, cancel);
+    return layout;
+  }
 
   private Component setupGrid(){
     grid = new Grid<>(User.class, false);
@@ -109,11 +225,15 @@ public class StudentListView extends MainViewFrame {
         .setHeader("Benutzername")
         .setResizable(true);
 
+    grid.addColumn(new ComponentRenderer<>(this::createDeleteButton))
+        .setHeader("")
+        .setResizable(true);
+
     students = createStudentList();
     GridListDataView<User> dataView = grid.setItems(students);
     searchField = new TextField();
     searchField.setWidth("50%");
-    searchField.setPlaceholder("Nach Schüler suchen");
+    searchField.setPlaceholder("Suche");
     searchField.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
     searchField.setValueChangeMode(ValueChangeMode.EAGER);
     searchField.addValueChangeListener(evt -> dataView.refreshAll());
@@ -132,7 +252,8 @@ public class StudentListView extends MainViewFrame {
       return matchesFullName || matchesEmail || matchesLastName || matchesUsername;
     });
 
-    VerticalLayout layout = new VerticalLayout(searchField, grid);
+    HorizontalLayout bar = new HorizontalLayout(searchField, addButtonDialog());
+    VerticalLayout layout = new VerticalLayout(bar, grid);
     layout.setPadding(false);
 
     return layout;
@@ -152,6 +273,15 @@ public class StudentListView extends MainViewFrame {
 
   private Component createUsername(User user){
     return new Label(user.getUsername() != null ? user.getUsername() : "");
+  }
+
+  private Component createDeleteButton(User user){
+    Button delete = new Button(VaadinIcon.TRASH.create());
+    delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+    delete.addClickListener(e -> {
+      userService.delete(user);
+    });
+    return delete;
   }
 
   protected List<User> executeFindAllUsers() {
